35a134344553444b70716cc09f0b5f0b7d86c54b
Sandeep Beniwal <Sandeep Beniwal7210@gmail.com>
2019-03-18 01:24:52 +0530

    string

---
+++ string_mnp.sh
@@ -0,0 +1,7 @@
+VAR="Sandeep Beniwal Kutani"
+echo full value of $VAR
+echo length of VAR is ${#VAR}
+echo print first 7 character ${VAR:0:7}
+echo print characters from 9 to 15 ${VAR:8:7}
+echo print characters from 4 to last ${VAR:4:30}
+echo print charcter from 2 to last ${VAR:2}
